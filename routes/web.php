9a40683645186e93109cfa182149fdedd0f2a289
Engsondos <engsondosahmed1@gmail.com>
2022-10-05 16:17:32 +0200

    test

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
-<<<<<<< HEAD
 use App\Http\Controllers\Articale;
-=======
 use App\Http\Controllers\Category;
->>>>>>> 401db726bf641e5a489e57b97055a872bad159ee
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,19 +15,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-<<<<<<< HEAD
 // Route::get('/', function () {
 //     return view('welcome');
 // });
 
 Route::get("articale/create", [Articale::class, "create"]);
-
 Route::post('articale/store', [Articale::class, "store"])->name("store");
-=======
 Route::get('Category/index', [Category::class, "index"]);
 Route::get('Category/create', [Category::class, "create"]);
 Route::post('Category/store', [Category::class, "store"]);
 Route::get('Category/edit/{id}', [Category::class, "edit"]);
 Route::get('Category/delete/{id}', [Category::class, "delete"]);
 Route::post('Category/update', [Category::class, "update"]);
->>>>>>> 401db726bf641e5a489e57b97055a872bad159ee
