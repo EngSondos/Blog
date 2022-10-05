82d6f5eab28d586b81bd1ced6e19d46ad2f0b2c5
MuhammedMagdyy <Mohamedmody04@gmail.com>
2022-10-05 14:09:19 +0200

    migrations

---
+++ database/migrations/2022_10_05_120735_create_comments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->string('guest_name');
+            $table->string('body');
+            $table->unsignedBigInteger('articale_id');
+            $table->foreign('articale_id')->references('id')->on('articales');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
